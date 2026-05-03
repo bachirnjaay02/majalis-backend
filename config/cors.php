948e3c08ed64bdd97427a507c1510af9eca24078
BASSIROUNDIAYE233 <bachirndiaye233@gmail.com>
2026-05-03 02:04:20 +0000

    fix:corrige cors.php

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://majalis-store.vercel.app/'],
+    'allowed_origins' => ['https://majalis-store.vercel.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
