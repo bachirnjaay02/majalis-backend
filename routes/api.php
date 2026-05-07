fc64d090b91c910609fd5a83542c309eb64ec89a
BASSIROUNDIAYE233 <bachirndiaye233@gmail.com>
2026-05-07 00:03:29 +0000

    fix: make products route public

--- routes/api.php
+++ routes/api.php
@@ -9,21 +9,18 @@ use App\Http\Controllers\Api\UserController;
 // Routes publiques
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
-
+Route::get('/products', [ProductController::class, 'index']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::put('/products/{product}', [ProductController::class, 'update']);
+Route::delete('/products/{product}', [ProductController::class, 'destroy']);
 
 // Produits
-    Route::get('/products', [ProductController::class, 'index']);
-    Route::post('/products', [ProductController::class, 'store']);
-    Route::put('/products/{product}', [ProductController::class, 'update']);
-    Route::delete('/products/{product}', [ProductController::class, 'destroy']);
-    
+   
+
 // Routes authentifiées
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'me']);
-
-    
-
     // Commandes
     Route::get('/orders', [OrderController::class, 'index']);
     Route::post('/orders', [OrderController::class, 'store']);
